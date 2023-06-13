192e82788b7e87d959caaa21ce71915b6d4d8c96
Mckeon-Campbell <mckeoncampbell@gmail.com>
2023-06-13 16:40:50 -0400

    Robot interface

---
+++ src/main/java/org/wcci/virtualpet/RobotInterface.java
@@ -0,0 +1,13 @@
+package org.wcci.virtualpet;
+
+public interface RobotInterface {
+    public int getOil();
+
+    public int getmT();
+
+    public boolean needsOil();
+
+    public boolean needsmT();
+
+    public void domT(int i);
+}

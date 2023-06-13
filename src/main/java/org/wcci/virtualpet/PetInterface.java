3bf3560dde0b049bdd803e489662319172148502
Mckeon-Campbell <mckeoncampbell@gmail.com>
2023-06-13 16:46:10 -0400

    Pet

---
+++ src/main/java/org/wcci/virtualpet/PetInterface.java
@@ -0,0 +1,15 @@
+package org.wcci.virtualpet;
+
+public interface PetInterface {
+    public void feed(int i);
+
+    public boolean isTired();
+
+    public Integer getHunger();
+
+    public boolean isHungry();
+
+    public boolean isOverfed();
+
+    
+}
